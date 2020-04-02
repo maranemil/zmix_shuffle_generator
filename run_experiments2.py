ddad7a60079ad35acf76b7fdd309f1b922bc8cc0
Emil Maran <maran.emil@gmail.com>
2020-04-02 23:58:16 +0200

    update

---
+++ run_experiments2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+from pysndfx import AudioEffectsChain
+from librosa import load
+
+from pydub import AudioSegment
+import os
+import random
+import time
+
+# pip install pysndfx
+# apt install sox
+##############################################
+#
+#  Read ~1 sec  / 25000 frames from WAV
+#
+##############################################
+import glob
+
+files = glob.glob('output/*')
+for f in files:
+    os.remove(f)
+
+# list spited files
+SPLIT_FOLDER = 'split/'
+files_list = [os.path.join(folder, i) for folder, subdirs, files in os.walk(SPLIT_FOLDER) for i in files]
+# files_list = files_list[7:19]  # from 7 to 19
+# files_list = files_list[:9]  # select first 7
+random.shuffle(files_list)
+print(files_list)
+
+
+# exit(0)
+
+
+def match_target_amplitude(sound, target_dBFS):
+    change_in_dBFS = target_dBFS - sound.dBFS
+    return sound.apply_gain(change_in_dBFS)
+
+
+octaves = 0.0  # 0.3 0.2
+for i in files_list:
+    time.sleep(2)
+    sound1 = AudioSegment.from_wav(random.choice(files_list))
+    sound2 = AudioSegment.from_wav(random.choice(files_list))
+    sound3 = AudioSegment.from_wav(random.choice(files_list))
+    sound4 = AudioSegment.from_wav(random.choice(files_list))
+    sound5 = AudioSegment.from_wav(random.choice(files_list))
+    sound6 = AudioSegment.from_wav(random.choice(files_list))
+
+    # sound1 = sound1.reverse()[-300:] * 2
+    # sound2 = sound2.reverse() - 3
+    # sound3 = sound3.fade_in(200).fade_out(200)
+    # sound4 = sound4.fade_in(200).fade_out(200)
+    # sound5 = sound5.reverse()
+    # sound6 = sound6.fade_in(200).fade_out(200).reverse()
+
+    sound1 = sound1.fade_in(100).fade_out(500).apply_gain_stereo(-1, +6).pan(-0.15)
+    sound2 = sound2.fade_in(100).fade_out(500).apply_gain_stereo(+6, +2).pan(+0.15)
+
+    new_sample_rate = int(sound3.frame_rate * (1.2 ** octaves)) # 1.1
+    hipitch_sound = sound3._spawn(sound3.raw_data, overrides={'frame_rate': new_sample_rate}).set_frame_rate(44100)
+    sound3 = hipitch_sound#.reverse()
+
+    new_sample_rate = int(sound4.frame_rate * (0.9 ** octaves)) # 2.6
+    hipitch_sound = sound4._spawn(sound4.raw_data, overrides={'frame_rate': new_sample_rate}).set_frame_rate(44100)
+    sound4 = hipitch_sound.apply_gain_stereo(+6, +1)  # .reverse()
+
+    new_sample_rate5 = int(sound5.frame_rate * (1.9 ** octaves))  # 1.2
+    hipitch_sound5 = sound5._spawn(sound5.raw_data, overrides={'frame_rate': new_sample_rate5}).set_frame_rate(44100)
+    sound5 = hipitch_sound5.apply_gain_stereo(+2, +6)  # .reverse()
+
+    sound6 = sound6.fade_in(400).fade_out(400).apply_gain_stereo(+2, +1)
+
+    combined_sounds = sound1 + sound2 + sound3 + sound4 + sound5 + sound6
+    filenameOutput = "output/output_" + time.strftime("%Y%m%d-%H%M%S") + ".wav"
+    normalized_sound = match_target_amplitude(combined_sounds, -15.0)  # normalized
+    normalized_sound.export(filenameOutput, format="wav")
+
+    fx = (
+        AudioEffectsChain()
+            # .highshelf()
+            .reverb()
+            .phaser()
+        # .delay()
+        # .lowshelf()
+    )
+
+    infile = filenameOutput
+    outfile = "output/output_" + os.path.basename(filenameOutput) + '.ogg'
+    # Apply phaser and reverb directly to an audio file.
+    fx(infile, outfile)
+    # Or, apply the effects directly to a ndarray.
+    y, sr = load(infile, sr=None)
+    y = fx(y)
+    # Apply the effects and return the results as a ndarray.
+    y = fx(infile)
+    # Apply the effects to a ndarray but store the resulting audio to disk.
+    fx(y, outfile)
