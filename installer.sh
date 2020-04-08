42333c772ed5ad57b769a7ca8b67e53197df292d
Emil Maran <maran.emil@gmail.com>
2020-04-08 12:50:37 +0200

    update

--- installer.sh
+++ installer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mkdir ~/Git && cd ~/Git
-sudo apt install git -y
-git clone https://github.com/maranemil/zmix_shuffle_generator
-cd zmix_shuffle_generator/
-
+#mkdir ~/Git && cd ~/Git
+#sudo apt install git -y
+#git clone https://github.com/maranemil/zmix_shuffle_generator
+#cd zmix_shuffle_generator/
 
 sudo apt install ffmpeg -y
 sudo apt install python3-pip -y
