cc6555619315a1656c16e482706ad8c6a67c9338
Emil Maran <maran.emil@gmail.com>
2020-04-10 10:58:57 +0200

    update

--- run_experiments1.py
+++ run_experiments1.py
@@ -23,11 +23,11 @@ def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
+
 # ----------------------------------------
 # Iterate
 # ----------------------------------------
 for i in files_list:
-
     time.sleep(2)
 
     # create new segments
